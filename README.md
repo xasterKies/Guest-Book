47d321fc398b7d108267da91c1b245e9491663db
xaster-Kies <samueledward40@gmail.com>
2021-12-10 23:19:55 +0100

    Post into a new entry with routing set

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Guest-Book
 **typeOfProject**  === [Scripting/Concept] <br />
-**Technologies:**  ExpressJs, ejs, bootstrap <br />
+**Technologies:**  ExpressJs, ejs, Javascript, twitter-bootstrap <br />
 **Description:**  1990s Guest book fantasies with express middlewares <br />
--- app.js
+++ app.js
@@ -2,8 +2,46 @@ var http = require("http");
 var path = require("path");
 var express = require("express")
 var logger = require("morgan")
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+const { response } = require("express");
 
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Creating a global array to store all entries
+var entries = []; 
+// Make entries available in all views
+app.locals.entries = entries;
+
+// Use morgan to log every request
+app.use(logger("dev"))
+
+// Populate the req.body variable if the user is submiting a form
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Routing handlers
+app.get("/", function(request, reponse) {
+    response.render("index")
+})
+
+app.get("/new-entry", function(request, response) {
+    response.render("new-entry")
+})
+
+app.post("/new-entry", function(request, response) {
+    if(!request.body.title || !request.body.body) {
+        response.status(400).send("Entries must have a title and a body.");
+    }
+    entries.push({
+        title: request.body.title,
+        content: request.body.body,
+        published: new Date()
+    })
+    response.redirect("/");
+})
+
+app.use(function(request, response) {
+    response.status(404).render("404");
+})
